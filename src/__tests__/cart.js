af4c746f9c8fca69f76519ab1b2bf44b3ba13816
Dan4te <ilya.burdueff@yandex.ru>
2024-12-04 19:09:21 +0300

    updatet

--- src/__tests__/cart.js
+++ src/__tests__/cart.js
@@ -1,24 +1,24 @@
-import { Cart } from '../components/Cart.js';
+import { Cart } from '../components/Cart.js'
 import products from '../samples/products.json';
 
 describe('cart module', () => {
     let cart;
     beforeEach(() => {
-        cart = new Cart(); 
+        cart = new Cart();
     });
-    test('can add products' , () => {
+    test('can add product', () => {
         cart.add(products.slice(0, 1));
 
         expect(cart.length).toBe(1);
     });
 
-    test('can add multiple products' , () => {
+    test('can add multiple products', () => {
         cart.add(products);
 
         expect(cart.length).toBe(2);
     });
-
-    test('can remove specific products' , () => {
+    
+    test('can remove specific products', () => {
         cart.add(products);
 
         expect(cart.length).toBe(2);
@@ -30,7 +30,7 @@ describe('cart module', () => {
         expect(cart.length).toBe(1);
     });
 
-    test('can be cleared' , () => {
+    test('can be cleared', () => {
         cart.add(products);
 
         expect(cart.length).toBe(2);
@@ -40,7 +40,7 @@ describe('cart module', () => {
         expect(cart.length).toBe(0);
     });
 
-    test('can calculate total summ' , () => {
+    test('can calculate total sum', () => {
         cart.add(products);
 
         expect(cart.length).toBe(2);
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,27 +1,27 @@
 export class Cart {
    #products = [];
 
-    constructor(options) {
-
-    }
-
-    add(products) {
-        this.#products.push(...products);
+   add(products) {
+       this.#products.push(...products);
    }
 
    remove(ids) {
-      this.#products = this.#products.filter(({ id }) => !ids.includes(id));
+       this.#products = this.#products.filter(({ id }) => !ids.includes(id));
    }
 
    clear() {
-      this.#products = [];
+       this.#products = [];
    }
 
+   get() {
+       return [...this.#products]
+   }
+   
    get sum() {
-      return this.#products.reduce((sum, { price }) => sum + price, 0);
+       return this.#products.reduce((sum, { price }) => sum + price, 0);
    }
 
-   get length() {
-      return this.#products.length
+   get length() { 
+       return this.#products.length
    }
 }
